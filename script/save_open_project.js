5ee5094239c02bb568e5ac9ec45ae2dc036fb70b
Laurence N. Béland <laurence.n.beland@gmail.com>
2018-08-21 11:30:27 -0400

    save_open_project (css + js)

--- script/save_open_project.js
+++ script/save_open_project.js
@@ -5,51 +5,103 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     let select_projet = document.getElementById("select_projet");
+    let options = document.getElementsByTagName("option");
+
     let btn_open = document.getElementById("btn_open");
-
-    let input_nom_projet = document.getElementById("input_nom_projet");
     let btn_save = document.getElementById("btn_save");
-
     let btn_remove = document.getElementById("btn_remove");
 
-    let list_saved_project;
+    // initialiser la liste des projets sauvegardés dès l'ouverture de la page
+    init_options();
 
     // INIT select list
-    function init(){
-
-        for(let p of localStorage){
-           // https://stackoverflow.com/questions/3138564/looping-through-localstorage-in-html5-and-javascript
+    function init_options(){
+        for(let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            //let value = localStorage[key];
+            //console.log(key);
+            let opt = document.createElement("option");
+            opt.value = key;
+            opt.innerHTML = key;
+            select_projet.appendChild(opt);
+            //if(value.equals(desired_value))
+            //    console.log(key + " => " + value);
         }
-
     }
 
-
     // SAVE PLAN
-    btn_save.addEventListener("click", function (evt, input_nom_projet) {
-
-        // effacer tous les projets de la liste
+    btn_save.addEventListener("click", function(evt) {
 
         let input = document.getElementById("input_nom_projet");
-        input_nom_projet = input.value;
-        console.log ("nom projet : ", input_nom_projet);
+        let select_projet = document.getElementById("select_projet");
 
+        console.log("select selected : ", select_projet.selected.value);
 
-        // localStorage Key, Value
-        localStorage.setItem("input_nom_projet", "value");
+        if(localStorage.getItem(input.value) !== null && input.value === "" && input.value === select_projet.selected) {
 
-        // pour le select, ajouter tous les projets
+            let new_svg = document.getElementById("svg_div").innerHTML;
+            localStorage.setItem(input.value, new_svg)
 
+        } else {
+
+            let input_nom_projet = input.value;
+            console.log ("nom projet : ", input_nom_projet);
+
+            let opt = document.createElement("option");
+
+            opt.value = input_nom_projet;
+            opt.textContent = input_nom_projet;
+
+            let svg = document.getElementById("svg_div").innerHTML;
+            console.log(svg);
+
+            // localStorage Key, Value
+            if (input_nom_projet.value === "") {
+                alert("Veuillez inscrire un nom de projet.");
+            } else {
+                localStorage.setItem(input_nom_projet, svg)
+                select_projet.add(opt, select_projet.options[select_projet.options.length + 1]);
+                alert("Votre projet est enregistré.");
+            }
+        }
     });
 
+    btn_open.addEventListener("click", function(evt){
 
-    btn_open.addEventListener("click", function(evt, selected_project){
 
-        //let projet = localStorage.getItem("selected_project");
+        for(let i = 1; i < options.length; i++){
+            if(options[i].selected){
+                let key = options[i].value;
+                console.log("ouvrir plan clické", key);
+                //console.log("options[o] : ", options[i]);
+                console.log("key :", key);
+                let value = localStorage.getItem(key);
 
+                document.getElementById("svg_div").innerHTML = value;
+                console.log("value : ", value);
+
+            }
+        }
     });
 
-    btn_remove.addEventListener("click", function(evt, selected_project){
-        localStorage.removeItem("selected_project");
+    btn_remove.addEventListener("click", function(evt){
+
+        for(let i = 1; i < options.length; i++) {
+            if(options[i].selected) {
+                let key = options[i].value;
+                localStorage.removeItem(key);
+            }
+        }
+        remove_projet_from_select();
+
     });
 
 });
+
+function remove_projet_from_select(){
+    let select_projet = document.getElementById("select_projet");
+    if(select_projet.options[0]){
+        select_projet.options[select_projet.selectedIndex] = null ;
+    }
+}
+
