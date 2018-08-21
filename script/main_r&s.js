fb35e76367d2619cab7b381e69454652d5849d48
Tahar Ben Smida <medtahar.bensmida@gmail.com>
2018-08-21 12:36:18 -0400

    Tahar : index et script du rotate

---
+++ script/main_r&s.js
@@ -0,0 +1,22 @@
+"use strict";
+let s = Snap("#svgout");
+
+
+let svg = document.getElementsByTagName("svg")[0];
+let circle = document.createElement("circle");
+
+
+let g = s.g();
+let img1 = g.image("images/chair.svg", 100, 100, 100, 100);
+
+g.dblclick(function (evt) {
+    let c = g.circle(200, 200, 5);
+    g.attr({id: 'toto'});
+
+    Draggable.create("#toto", {
+        type: "rotation",
+        throwProps: true
+    })
+});
+
+
